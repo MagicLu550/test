5e06f9aa5d4609a5d2263d1d56ed0da1763cd2c8
magiclu550 <843983728@qq.com>
2020-04-08 07:29:19 +0800

    Initial commit

---
+++ src/main/java/cn/textwar/test/Test.java
@@ -0,0 +1,67 @@
+package cn.textwar.test;
+
+import cn.textwar.test.dsl.DSLMethod;
+import cn.textwar.test.dsl.DSLParser;
+import groovy.lang.Closure;
+
+import java.io.File;
+
+public class Test extends DSLParser {
+
+    public Test(File file) {
+        super(file);
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test(new File("server.cfg"));
+    }
+
+    /**
+     * server标签，为根标签
+     */
+    @DSLMethod
+    public void server(Closure closure){
+        closure.call();
+    }
+
+    /**
+     *负责rpc的属性
+     */
+    @DSLMethod
+    public void rpc(Closure closure){
+        closure.call();
+    }
+
+    /**
+     * 负责配置数据库
+     */
+    @DSLMethod
+    public void database(Closure closure){
+        closure.call();
+    }
+
+    @DSLMethod
+    public void game(Closure closure){
+        closure.call();
+    }
+
+    @DSLMethod
+    public void python(Closure closure){
+        closure.call();
+    }
+
+    @DSLMethod
+    public void player(Closure closure){
+        closure.call();
+    }
+
+    @DSLMethod
+    public void born(Closure closure){
+        closure.call();
+    }
+
+    @DSLMethod
+    public void map(Closure closure){
+        closure.call();
+    }
+}
---
+++ src/main/java/cn/textwar/test/dsl/DSLParser.groovy
@@ -0,0 +1,93 @@
+package cn.textwar.test.dsl
+
+import groovy.transform.Memoized
+
+import java.lang.reflect.Method
+
+/**
+ * @author MagicLu550 @ 卢昶存
+ */
+abstract class DSLParser {
+
+    GroovyClassLoader loader
+
+    Class fileClass
+
+    Map<String, Method> methodMap = [:]
+
+    Map<String,Object[]> entry = [:]
+
+    private String file
+
+    DSLParser(File file){
+        this(new FileInputStream(file))
+        this.file = file.toString()
+    }
+
+    DSLParser(InputStream inputStream){
+        this.loader = new GroovyClassLoader(DSLParser.class.getClassLoader())
+        this.fileClass = loader.parseClass(inputStream.text)
+        this.fileClass.metaClass.methodMissing = {
+            String name,args->
+                def nowName = new StringBuilder()
+                def allNames = []
+                new Throwable().stackTrace.toList().each{
+                    x->
+                        if(x.toString().startsWith(this.class.name)){
+                            def strs = x.toString().split("\\.")
+                            def str = strs[strs.length-2]
+                            str = str.substring(0,str.indexOf("("))
+                            if(!(str == "doCall")&&!(str == "<init>")&&!(str == "main")){
+                                allNames.add(str)
+                            }
+                        }
+                }
+                for(int i = allNames.size()-1;i>=0;i--){
+                    nowName.append(allNames[i]).append(".")
+                }
+                nowName = nowName.substring(0,nowName.lastIndexOf("."))
+                entry["${nowName}.${name}"] = args
+        }
+        this.loadDSLMethod()
+        this.fileClass.getMethod("run").invoke(fileClass.newInstance())
+    }
+
+    /**
+     * 这里用于获取相应的元素，返回全部参数
+     * 如
+     * <code>
+     *     rpc{
+     *         server "name","hello"
+     *         //名称 rpc.server 参数为name和hello
+     *         //默认值如果没有这个属性，默认返回的数值
+     *     }
+     * </code>
+     */
+    @Memoized
+    Object[] getValue(String key,Object defaultValue){
+        def t = entry[key]
+        t==null?[defaultValue]:t
+    }
+
+    /**
+     *这个是简化了操作，只能获取一个参数的，多个可能会报错
+     * 并且参数必须是字符串类型
+     */
+    @Memoized
+    String getHeadValue(String key){
+        return getValue(key,"")[0]
+    }
+
+    void loadDSLMethod(){
+        this.class.methods.toList().each{
+            x->
+                if(x.getAnnotation(DSLMethod.class)){
+                    this.methodMap[x.name] = x
+                    this.fileClass.metaClass."${x.name}" = {
+                        Closure closure->
+                            x.invoke(this,closure)
+                    }
+                }
+        }
+    }
+}
